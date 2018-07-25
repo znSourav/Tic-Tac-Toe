2e456aaad5fbf6ae7a6a9fa49009466352ed9934
znSourav <zannatulnaimsourav@gmail.com>
2018-07-26 02:42:00 +0600

    LC

--- Tic Tac Toe/Form1.cs
+++ Tic Tac Toe/Form1.cs
@@ -198,6 +198,12 @@ namespace Tic_Tac_Toe
 
                 }
             }
+            else if(button1.Enabled==false && button2.Enabled==false && button3.Enabled == false && button4.Enabled == false && button5.Enabled == false && button6.Enabled == false && button7.Enabled == false && button8.Enabled == false && button9.Enabled == false)
+            {
+                PlayerP.Text = "Game Draws";
+                SignLabel.Text = "";
+                ReplayButton.Enabled = true;
+            }
 
         }
 
